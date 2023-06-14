7600859ba0a5e579686ce447f10b27817f396850
Anton Hakansson <anton.hakansson98@gmail.com>
2023-06-14 15:43:20 +0200

    feat(hk): home combo mod

--- keyboards/annepro2/keymaps/hakanssn/config.h
+++ keyboards/annepro2/keymaps/hakanssn/config.h
@@ -1,4 +1,8 @@
+#undef PERMISSIVE_HOLD
 #define TAPPING_TERM 150
-#define TAPPING_TERM_HOME_ROW 600
-#define TAPPING_TERM_PER_KEY
 #define IGNORE_MOD_TAP_INTERRUPT
+
+#define COMBO_VARIABLE_LEN   // compute COMBO_LEN from combos.def
+#define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
+#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
