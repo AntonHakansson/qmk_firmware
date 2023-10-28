179c614bead6ab3dfac9ab1e53c40514df33014b
Anton Hakansson <anton.hakansson98@gmail.com>
2023-10-28 17:19:39 +0200

    fix(hk): IGNORE_MOD_TAP_INTERRUPT is already default

--- keyboards/annepro2/keymaps/hakanssn/config.h
+++ keyboards/annepro2/keymaps/hakanssn/config.h
@@ -1,6 +1,5 @@
 #undef PERMISSIVE_HOLD
 #define TAPPING_TERM 150
-#define IGNORE_MOD_TAP_INTERRUPT
 
 #define COMBO_VARIABLE_LEN   // compute COMBO_LEN from combos.def
 #define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
