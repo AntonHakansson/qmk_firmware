1b293a84a04b5c7b3d65ca9747b31014e6c42d93
Anton Hakansson <anton.hakansson98@gmail.com>
2024-08-13 21:36:05 +0200

    refactor(hk): abstract user code to separate file

--- keyboards/splitkb/kyria/keymaps/hk/hk_keymap.h
+++ keyboards/splitkb/kyria/keymaps/hk/hk_keymap.h
@@ -5,17 +5,12 @@ enum layers {
     _NAV,
     _SYM,
     _FUNCTION,
-    _ADJUST,
 };
 
-// Aliases for readability
-#define COLEMAK  DF(_COLEMAK_DH)
-
+//- Readability Aliases
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
 #define FKEYS    MO(_FUNCTION)
-#define ADJUST   MO(_ADJUST)
-
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define LALT_TAB  MT(MOD_LALT, KC_TAB)
@@ -24,3 +19,15 @@ enum layers {
 #define RSFT_ENT  MT(MOD_LSFT, KC_ENT)
 #define OS_LSFT  OSM(MOD_LSFT)
 #define OS_RSFT  OSM(MOD_RSFT)
+
+//- Layout_split_5x3_3
+#define __________________ROW_L1__________________ KC_Q  ,  KC_W  ,  KC_F    ,  KC_P   , KC_B
+#define __________________ROW_L2__________________ KC_A  ,  KC_R  ,  KC_S    ,  KC_T   , KC_G
+#define __________________ROW_L3__________________ KC_Z  ,  KC_X  ,  KC_C    ,  KC_D   , KC_V
+
+#define __________________ROW_R1__________________ KC_J  ,  KC_L  ,  KC_U    ,  KC_Y   , KC_SCLN
+#define __________________ROW_R2__________________ KC_M  ,  KC_N  ,  KC_E    ,  KC_I   , KC_O
+#define __________________ROW_R3__________________ KC_K  ,  KC_H  , KC_COMM  , KC_DOT  , KC_SLSH
+
+#define __________________THUMB_L__________________ LALT_TAB,LCTL_SPC, NAV
+#define __________________THUMB_R__________________ SYM     ,RSFT_ENT,LALT_BSPC
